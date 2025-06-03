a9096c05a156f01332f5b0955e6275fd639efa6c
RafidH777 <124240060@student.upnyk.ac.id>
2025-06-03 13:13:25 +0700

    fix buy button problem

--- pages/marketplace.php
+++ pages/marketplace.php
@@ -17,7 +17,7 @@ $result = mysqli_query($koneksi, $sql);
 
 <?php include '../includes/header.php' ?>
 
-<h1 class="mb-4 heading">Marketplace</h1>
+<h1 class="mb-4 heading" style="color: var(--color-text)">Marketplace</h1>
 <div class="row">
   <?php while ($item = mysqli_fetch_assoc($result)) : ?>
     <div class="col-md-4 mb-3">
@@ -30,16 +30,16 @@ $result = mysqli_query($koneksi, $sql);
           <p class="card-text"><strong>Price:</strong> <?= $item['price'] ?></p>
           <p class="card-text"><strong>Quantity:</strong> <?= $item['sale_quantity'] ?></p>
           <input type="checkbox" name="buy-toggle" id="buy-toggle-<?= $item['item_id'] ?>" hidden>
-          <label class="buy-toggle-label text-center" for="buy-toggle-<?= $item['item_id'] ?>" class="btn btn-warning mb-2" style="cursor: pointer;"></label>
-          <form method="POST" action="../actions/buy.php" class="d-flex gap-5">
-            <input type="hidden" name="item_id" value="<?= $item['item_id'] ?>">
-            <input type="hidden" name="price" value="<?= $item['price'] ?>">
-            <input type="hidden" name="seller_id" value="<?= $item['user_id'] ?>">
-            <input type="number" name="quantity" id="quantity" max="$item['sale_quantity']" class="form-control" placeholder="Quantity">
-            <?php if ($item['user_id'] !== $_SESSION['user_id']) : ?>
+          <label for="buy-toggle-<?= $item['item_id'] ?>" class="btn btn-warning mb-2 buy-toggle-label text-center" style="cursor: pointer;"></label>
+          <?php if ($item['user_id'] !== $_SESSION['user_id']) : ?>
+            <form method="POST" action="../actions/buy.php" class="d-flex gap-5">
+              <input type="hidden" name="item_id" value="<?= $item['item_id'] ?>">
+              <input type="hidden" name="price" value="<?= $item['price'] ?>">
+              <input type="hidden" name="seller_id" value="<?= $item['user_id'] ?>">
+              <input type="number" name="quantity" id="quantity" max="$item['sale_quantity']" class="form-control" placeholder="Quantity">
               <button class="btn btn-register" type="submit">Confirm</button>
             <?php endif; ?>
-          </form>
+            </form>
         </div>
       </div>
     </div>
